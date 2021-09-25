da158f07ec85a6d99f920e62611dc185e8ac781e
Shruti Dhongade <shrutisdhongade1605@gmail.com>
2021-09-25 17:54:08 +0530

    Python Hackerrank Solutions

---
+++ Python/1. Introduction/2. Python If-Else.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if (n%2==0):
+        if (5>=n>=2):
+            print("Not Weird")
+        elif(20>=n>=6):
+            print("Weird")
+        elif(n>20):    
+            print("Not Weird")
+    else:
+        print("Weird")
